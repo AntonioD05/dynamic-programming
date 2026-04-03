313ff4f8f951be4d7a5ea24b84856e50099f6259
Antonio Diaz <antoniodiaz1@ufl.edu>
2026-04-03 19:12:18 -0400

    Implement dynamic programming solution for HVLCS

--- src/hvlcs.py
+++ src/hvlcs.py
@@ -1,4 +1,5 @@
 import sys
+
 def read_input(filename):
     with open(filename, "r") as f:
         lines = [line.strip() for line in f if line.strip()]
@@ -15,7 +16,41 @@ def read_input(filename):
 
     return values, A, B
 
-values, A, B = read_input(sys.argv[1])
-print(values)
-print(A)
-print(B)
+def compute_hvlcs(values, A, B):
+    n = len(A)
+    m = len(B)
+
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+    for i in range(n - 1, -1, -1):
+        for j in range(m - 1, -1, -1):
+            dp[i][j] = max(dp[i + 1][j], dp[i][j + 1])
+
+            if A[i] == B[j]:
+                dp[i][j] = max(dp[i][j], values[A[i]] + dp[i + 1][j + 1])
+
+    subsequence = []
+    i, j = 0, 0
+
+    while i < n and j < m:
+        if A[i] == B[j] and dp[i][j] == values[A[i]] + dp[i + 1][j + 1]:
+            subsequence.append(A[i])
+            i += 1
+            j += 1
+        elif dp[i + 1][j] >= dp[i][j + 1]:
+            i += 1
+        else:
+            j += 1
+
+    return dp[0][0], "".join(subsequence)
+
+def main():
+    filename = sys.argv[1]
+    values, A, B = read_input(filename)
+    max_value, subsequence = compute_hvlcs(values, A, B)
+
+    print(max_value)
+    print(subsequence)
+
+if __name__ == "__main__":
+    main()
